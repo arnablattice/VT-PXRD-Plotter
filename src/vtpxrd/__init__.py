09562ca326f547ff56201492509207d7042a477d
Arnab Dutta <duttarnab97@gmail.com>
2025-08-08 18:26:41 +0530

    __init__.py

--- src/vtpxrd/__init__.py
+++ src/vtpxrd/__init__.py
@@ -1,10 +1,10 @@
-version = "0.1.0"
+__version__ = '0.1.0'
 
 """
 VT-PXRD-Plotter: A lightweight Python package for plotting and analyzing
 variable-temperature powder X-ray diffraction (VT-PXRD) data.
 
 Author: Arnab Dutta
-License: BSD-3-Clause
+License: MIT
 Repository: https://github.com/arnablattice/VT-PXRD-Plotter
 """
